3d3fe9b98728012c04834692c06be534007e0840
Brian Cardarella <bcardarella@gmail.com>
2012-01-22 06:31:08 -0500

    Presence validator

--- lib/client_side_validations/mongoid.rb
+++ lib/client_side_validations/mongoid.rb
@@ -2,7 +2,7 @@ require 'client_side_validations/active_model'
 require 'client_side_validations/middleware'
 require 'client_side_validations/mongoid/middleware'
 
-%w{uniqueness}.each do |validator|
+%w{presence uniqueness}.each do |validator|
   require "client_side_validations/mongoid/#{validator}"
   validator.capitalize!
   eval "Mongoid::Validations::#{validator}Validator.send(:include, ClientSideValidations::Mongoid::#{validator})"
