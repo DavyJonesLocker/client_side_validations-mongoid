3ed83a62d2693249422c4031c74b9271f80b5f25
Brian Cardarella <bcardarella@gmail.com>
2012-10-08 19:09:53 -0400

    Fixed test suite

--- test/mongoid/models/book.rb
+++ test/mongoid/models/book.rb
@@ -4,6 +4,8 @@ class Book
   field :age, :type => Integer
   field :author_name, :type => String
   field :author_email, :type => String
+
+  validates :author_email, :author_name, :uniqueness => true
 end
 
 module MongoidTestModule
