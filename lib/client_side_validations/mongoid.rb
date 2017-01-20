03476306cc2668a36069032db38d198fb39df501
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-20 14:43:39 +0100

    Improve code

--- lib/client_side_validations/mongoid.rb
+++ lib/client_side_validations/mongoid.rb
@@ -1,13 +1,10 @@
-module ClientSideValidations; end
-
 require 'client_side_validations/active_model'
 require 'client_side_validations/middleware'
 require 'client_side_validations/mongoid/middleware'
 
-%w(presence uniqueness).each do |validator|
-  require "client_side_validations/mongoid/#{validator}"
-  validator.capitalize!
+ClientSideValidations::Middleware::Uniqueness.register_orm(ClientSideValidations::Mongoid::Middleware)
+
+%w(Presence Uniqueness).each do |validator|
+  require "client_side_validations/mongoid/#{validator.downcase}"
   Mongoid::Validatable.const_get("#{validator}Validator").send :include, ClientSideValidations::Mongoid.const_get(validator)
 end
-
-ClientSideValidations::Middleware::Uniqueness.register_orm(ClientSideValidations::Mongoid::Middleware)
