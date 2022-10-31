be3b285b46063b80c58d82b761f4e8b10ec7b1e3
Geremia Taglialatela <tagliala.dev@gmail.com>
2022-10-31 08:23:49 +0100

    Update dependencies

--- test/mongoid/cases/test_presence_validator.rb
+++ test/mongoid/cases/test_presence_validator.rb
@@ -6,11 +6,13 @@ module Mongoid
   class PresenceValidatorTest < ClientSideValidations::MongoidTestBase
     def test_presence_client_side_hash
       expected_hash = { message: "can't be blank" }
+
       assert_equal expected_hash, PresenceValidator.new(attributes: [:name]).client_side_hash(@book, :age)
     end
 
     def test_presence_client_side_hash_with_custom_message
       expected_hash = { message: 'is required' }
+
       assert_equal expected_hash, PresenceValidator.new(attributes: [:name], message: 'is required').client_side_hash(@book, :age)
     end
   end
--- test/mongoid/cases/test_uniqueness_validator.rb
+++ test/mongoid/cases/test_uniqueness_validator.rb
@@ -6,16 +6,19 @@ module Mongoid
   class UniqunessValidatorTest < ClientSideValidations::MongoidTestBase
     def test_uniqueness_client_side_hash
       expected_hash = { message: 'has already been taken' }
+
       assert_equal expected_hash, UniquenessValidator.new(attributes: [:name]).client_side_hash(@book, :age)
     end
 
     def test_uniqueness_client_side_hash_allowing_blank
       expected_hash = { message: 'has already been taken', allow_blank: true }
+
       assert_equal expected_hash, UniquenessValidator.new(attributes: [:author_name], allow_blank: true).client_side_hash(@book, :author_name)
     end
 
     def test_uniqueness_client_side_hash_with_custom_message
       expected_hash = { message: 'is not available' }
+
       assert_equal expected_hash, UniquenessValidator.new(attributes: [:name], message: 'is not available').client_side_hash(@book, :age)
     end
 
@@ -23,6 +26,7 @@ module Mongoid
       @book.stubs(:new_record?).returns(false)
       @book.stubs(:id).returns(1)
       expected_hash = { message: 'has already been taken', id: 1 }
+
       assert_equal expected_hash, UniquenessValidator.new(attributes: [:name]).client_side_hash(@book, :age)
     end
 
@@ -30,6 +34,7 @@ module Mongoid
       @book.stubs(:author_email).returns('test@test.com')
       expected_hash = { message: 'has already been taken', scope: { author_email: 'test@test.com' } }
       result_hash = UniquenessValidator.new(attributes: [:author_name], scope: :author_email).client_side_hash(@book, :author_name)
+
       assert_equal expected_hash, result_hash
     end
 
@@ -38,18 +43,21 @@ module Mongoid
       @book.stubs(:author_email).returns('test@test.com')
       expected_hash = { message: 'has already been taken', scope: { age: 30, author_email: 'test@test.com' } }
       result_hash = UniquenessValidator.new(attributes: [:author_name], scope: %i[age author_email]).client_side_hash(@book, :author_name)
+
       assert_equal expected_hash, result_hash
     end
 
     def test_uniqueness_client_side_hash_with_empty_scope_array
       expected_hash = { message: 'has already been taken' }
       result_hash = UniquenessValidator.new(attributes: [:author_name], scope: []).client_side_hash(@book, :author_name)
+
       assert_equal expected_hash, result_hash
     end
 
     def test_uniqueness_client_side_hash_when_nested_module
       @book = MongoidTestModule::Book2.new
       expected_hash = { message: 'has already been taken', class: 'mongoid_test_module/book2' }
+
       assert_equal expected_hash, UniquenessValidator.new(attributes: [:name]).client_side_hash(@book, :age)
     end
   end
